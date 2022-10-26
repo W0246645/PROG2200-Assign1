3b83626d079f749044b2b5832b553a4e676bf372
Matthew Covey <w0246645@campus.nscc.ca>
2022-10-26 13:59:47 -0300

    Restructured Project

--- Chat/Chat/Properties/AssemblyInfo.cs
+++ Chat/Chat/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClientApp")]
+[assembly: AssemblyTitle("Chat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClientApp")]
+[assembly: AssemblyProduct("Chat")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b0c831a-6643-4f64-a277-1797936c05b6")]
+[assembly: Guid("388e28f1-20a7-4b87-8eff-7afb919897ab")]
 
 // Version information for an assembly consists of the following four values:
 //
