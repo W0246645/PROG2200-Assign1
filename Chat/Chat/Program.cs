8060f43a4bb594302887a77691c72787064ad27a
Matthew Covey <w0246645@campus.nscc.ca>
2022-11-01 08:51:43 -0300

    Completed assignment, video submitted.

--- Chat/Chat/Program.cs
+++ Chat/Chat/Program.cs
@@ -5,6 +5,7 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Chat
 {
@@ -12,80 +13,16 @@ namespace Chat
     {
         public static void Main(string[] args)
         {
+            //Runs the program as the server or client.
             if (args.Contains("-server"))
             {
-                Console.WriteLine("Server");
-                //var server = new Server();
-
-                TcpListener server = null;
-                try
-                {
-                    // Set the TcpListener on port 13000.
-                    Int32 port = 13000;
-                    IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-                    // TcpListener server = new TcpListener(port);
-                    server = new TcpListener(localAddr, port);
-
-                    // Start listening for client requests.
-                    server.Start();
-
-                    // Buffer for reading data
-                    Byte[] bytes = new Byte[256];
-                    String data = null;
-
-                    // Enter the listening loop.
-                    while (true)
-                    {
-                        Console.Write("Waiting for a connection... ");
-
-                        // Perform a blocking call to accept requests.
-                        // You could also use server.AcceptSocket() here.
-                        TcpClient client = server.AcceptTcpClient();
-                        Console.WriteLine("Connected!");
-
-                        data = null;
-
-                        // Get a stream object for reading and writing
-                        NetworkStream stream = client.GetStream();
-
-                        int i;
-
-                        // Loop to receive all the data sent by the client.
-                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                        {
-                            // Translate data bytes to a ASCII string.
-                            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                            Console.WriteLine("Received: {0}", data);
-
-                            // Process the data sent by the client.
-                            data = data.ToUpper();
-
-                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                            // Send back a response.
-                            stream.Write(msg, 0, msg.Length);
-                            Console.WriteLine("Sent: {0}", data);
-                        }
-
-                        // Shutdown and end the connection
-                        client.Close();
-                    }
-                }
-                catch (SocketException e)
-                {
-                    Console.WriteLine("SocketException: {0}", e);
-                }
-                finally
-                {
-                    server.Stop();
-                }
+                ServerStart();
             }
             else
             {
                 Console.WriteLine("Client");
                 //var client = new Client();
-                Connect("127.0.0.1");
+                ClientConnect("127.0.0.1");
             }
 
 
@@ -93,7 +30,111 @@ namespace Chat
             Console.Read();
         }
 
-        static void Connect(String server)
+        static void ServerStart()
+        {
+            Console.WriteLine("Server");
+            //var server = new Server();
+
+            TcpListener server = null;
+            try
+            {
+                var quit = false;
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] data = new Byte[256];
+                TcpClient client = null;
+
+                // Enter the listening loop.
+                while (!quit)
+                {
+                    if (client == null)
+                    {
+                        Console.Write("Waiting for a connection... ");
+
+                        // Perform a blocking call to accept requests.
+                        // You could also use server.AcceptSocket() here.
+                        client = server.AcceptTcpClient();
+                        Console.WriteLine("Connected!");
+                    }
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(true);
+                        var message = "";
+
+                        if (key.Key == ConsoleKey.I)
+                        {
+                            Console.Write("Insertion Mode>>");
+                            message = Console.ReadLine();
+
+                            // Translate the passed message into ASCII and store it as a Byte array.
+                            byte[] msg = Encoding.ASCII.GetBytes(message);
+
+                            // Send the message to the connected TcpServer.
+                            stream.Write(msg, 0, msg.Length);
+                        }
+                    }
+                    if (stream.DataAvailable)
+                    {
+                        // String to store the response ASCII representation.
+                        string responseData = string.Empty;
+
+                        // Read the first batch of the TcpServer response bytes.
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                        Console.WriteLine(responseData);
+                    }
+
+                    // Detect if client disconnected. Not very graceful but it is functional.
+                    try
+                    {
+                        if (client.Client.Poll(0, SelectMode.SelectRead))
+                        {
+                            byte[] buff = new byte[1];
+                            if (client.Client.Receive(buff, SocketFlags.Peek) == 0)
+                            {
+                                // Client disconnected
+                                quit = true;
+                            }
+                        }
+                    } catch
+                    {
+                        Console.WriteLine("Application has been disconnected");
+                        quit = true;
+                    }
+
+                    if (quit)
+                    {
+                        // Shutdown and end the connection
+                        client.Close();
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
+        static void ClientConnect(string server)
         {
             try
             {
@@ -109,43 +150,76 @@ namespace Chat
 
                 // Get a client stream for reading and writing.
                 NetworkStream stream = client.GetStream();
+                Console.WriteLine("Client is connected to Server!");
 
                 while (!quit)
                 {
-                    var key = Console.ReadKey();
-                    var message = "";
-                    if (key.Key == ConsoleKey.Escape)
+                    Byte[] data;
+                    if (Console.KeyAvailable)
                     {
-                        quit = true;
-                        Console.WriteLine("You typed Escape to exit.");
-                        message = "Disconnected\nGoodbye!";
+                        var key = Console.ReadKey(true);
+                        var message = "";
+                        
+                        if (key.Key == ConsoleKey.Escape)
+                        {
+                            quit = true;
+                            Console.WriteLine("You hit Escape key to exit.");
+                        }
+                        else if (key.Key == ConsoleKey.I)
+                        {
+                            Console.Write("Insertion Mode>>");
+                            message = Console.ReadLine();
+                            if (message == "quit")
+                            {
+                                quit = true;
+                                Console.WriteLine("You typed \"quit\" to exit.");
+                            }
+                            else
+                            {
+
+                                // Translate the passed message into ASCII and store it as a Byte array.
+                                data = Encoding.ASCII.GetBytes(message);
+
+                                // Send the message to the connected TcpServer.
+                                stream.Write(data, 0, data.Length);
+                            }
+                        }
                     }
-                    else if (key.Key == ConsoleKey.I)
+                    // Buffer to store the response bytes.
+                    data = new Byte[256];
+
+                    if (stream.DataAvailable)
                     {
-                        Console.Write("Insertion Mode>>");
-                        message = Console.ReadLine();
-                    }
-                    // Translate the passed message into ASCII and store it as a Byte array.
-                        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                        // Send the message to the connected TcpServer.
-                        stream.Write(data, 0, data.Length);
-
-                        Console.WriteLine("Sent: {0}", message);
-
-                        // Receive the server response.
-
-                        // Buffer to store the response bytes.
-                        data = new Byte[256];
 
                         // String to store the response ASCII representation.
-                        String responseData = String.Empty;
+                        string responseData = string.Empty;
 
                         // Read the first batch of the TcpServer response bytes.
                         Int32 bytes = stream.Read(data, 0, data.Length);
-                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                        Console.WriteLine("Received: {0}", responseData);
-                }       
+                        responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                        Console.WriteLine(responseData);
+                    }
+
+                    // Detect if client disconnected. Not very graceful but it is functional.
+                    try
+                    {
+                        if (client.Client.Poll(0, SelectMode.SelectRead))
+                        {
+                            byte[] buff = new byte[1];
+                            if (client.Client.Receive(buff, SocketFlags.Peek) == 0)
+                            {
+                                // Client disconnected
+                                quit = true;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        quit = true;
+                    }
+                }
+
+                Console.WriteLine("Disconnected\nGood Bye!");
 
                 // Explicit close is not necessary since TcpClient.Dispose() will be
                 // called automatically.
@@ -160,9 +234,6 @@ namespace Chat
             {
                 Console.WriteLine("SocketException: {0}", e);
             }
-
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
         }
     }
 }
